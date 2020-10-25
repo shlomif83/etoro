98ae69812963ccdf70d3b1dd576cb7bec52904aa
Shlomo Farber <shlomof@ravtech.co.il>
2020-10-25 20:49:27 +0200

    finish code

--- print_job_name.py
+++ print_job_name.py
@@ -2,6 +2,10 @@ import os
 
 job_number = int(os.environ['BUILD_NUMBER'])
 
-assert job_number % 3 == 0, "build number are not modulue in 3"
 
-print(os.environ['JOB_NAME'])
+def print_job_name():
+    assert job_number % 3 == 0, "build number is not % in 3"
+    print(os.environ['JOB_NAME'])
+
+if __name__ == "__main__":
+    print_job_name()
