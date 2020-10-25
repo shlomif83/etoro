6c760ac78068457d55c30a0c2f9080282a3ef925
Shlomo Farber <shlomof@ravtech.co.il>
2020-10-25 20:52:23 +0200

    finish code

--- print_job_name.py
+++ print_job_name.py
@@ -5,7 +5,7 @@ job_number = int(os.environ['BUILD_NUMBER'])
 
 def print_job_name():
     assert job_number % 3 == 0, "build number is not % in 3"
-    print(os.environ['JOB_NAME'])
+    print("job name is:" + os.environ['JOB_NAME'])
 
 if __name__ == "__main__":
     print_job_name()
