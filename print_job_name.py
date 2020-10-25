20f0babc0cc56d1b9e93f793972fda8cd3a38a6f
Shlomo Farber <shlomof@ravtech.co.il>
2020-10-25 14:53:44 +0200

    rename env

--- print_job_name.py
+++ print_job_name.py
@@ -2,5 +2,6 @@ import os
 
 job_number = int(os.environ['BUILD_NUMBER'])
 
-if job_number % 3 == 0:
-    print(os.environ['JOB_NAME'])
+assert job_number % 3 == 0, "build number are not modulue in 3"
+# if job_number % 3 == 0:
+print(os.environ['JOB_NAME'])
